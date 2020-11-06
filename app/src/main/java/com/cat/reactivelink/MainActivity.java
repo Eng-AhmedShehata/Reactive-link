147bd3b4b813f3e76eb987f4fca27a0ae90999e7
ahamdyshehata7@gmail.com <ahamdyshehata7@gmail.com>
2020-11-06 20:21:56 +0200

    add setup code and needed dependencies

--- app/src/main/java/com/cat/reactivelink/MainActivity.java
+++ app/src/main/java/com/cat/reactivelink/MainActivity.java
@@ -10,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // TODO Here, we test our library like individual project
+
     }
 }
